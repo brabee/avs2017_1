8c6d7fbe8f1bae3bff395e9576b5e84d7361610a
brabee <tomasbrabeden@gmail.com>
2017-03-28 22:39:23 +0200

    'test'

--- resources/views/auth/loginmodal.blade.php
+++ resources/views/auth/loginmodal.blade.php
@@ -32,10 +32,10 @@
 						{{ csrf_field() }}
 						<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
 							<label  class="col-sm-2 control-label"
-							        for="inputEmail3">Email</label>
+							        for="email">Email</label>
 							<div class="col-sm-10">
-								<input type="email" class="form-control"
-								       id="inputEmail3" value="{{ old('email') }}" placeholder="Email" required autofocus>
+								<input type="email"  name="email" class="form-control"
+								       id="email" value="{{ old('email') }}" placeholder="Email" required autofocus>
 								@if ($errors->has('email'))
 									<span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -45,10 +45,10 @@
 						</div>
 						<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
 							<label class="col-sm-2 control-label"
-							       for="inputPassword3" >Heslo</label>
+							       for="password" >Heslo</label>
 							<div class="col-sm-10">
-								<input type="password" class="form-control"
-								       id="inputPassword3" placeholder="Password" required>
+								<input type="password" class="form-control" name="password"
+								       id="password" placeholder="Password" required>
 
 								@if ($errors->has('password'))
 									<span class="help-block">
